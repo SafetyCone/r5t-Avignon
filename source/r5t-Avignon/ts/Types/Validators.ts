39b3f656238c041f38ed9393e78e680279d1db12
David Coats <david@getsafetycone.com>
2021-04-26 00:33:55 -0700

    Add synchronous validator type

--- source/r5t-Avignon/ts/Types/Validators.ts
+++ source/r5t-Avignon/ts/Types/Validators.ts
@@ -4,3 +4,5 @@ export type Validator<T> = (value: T) => Promise<ValidationResult>
 export type ObjectValidator = Validator<any>;
 export type StringValidator = Validator<string>;
 export type NumberValidator = Validator<number>;
+
+export type SynchronousValidator<T> = (value: T) => ValidationResult;
